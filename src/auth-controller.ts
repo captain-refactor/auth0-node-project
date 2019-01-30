30ff591fb45222dbd73048b0ba24df032cf817cc
Jan Kremen <kremenh@gmail.com>
2019-01-30 22:51:12 +0100

    completing auth

--- src/auth-controller.ts
+++ src/auth-controller.ts
@@ -1,9 +1,17 @@
 import {AuthenticationClient} from "auth0";
 import * as Router from "koa-router";
 import * as bodyParser from "koa-bodyparser";
+import jwt = require('koa-jwt');
+import jwks = require('jwks-rsa');
+
+export interface AuthConfig {
+    issuer: string;
+    audience: string;
+    jwksUri: string
+}
 
 export class AuthController {
-    constructor(private client: AuthenticationClient) {
+    constructor(private client: AuthenticationClient, private config: AuthConfig) {
     }
 
     async login(username: string, password: string) {
@@ -11,16 +19,46 @@ export class AuthController {
     }
 
     router(): Router {
+        const cookie = 'token';
+        const checkJwt = jwt({
+            // @ts-ignore
+            secret: jwks.koaJwtSecret({
+                cache: true,
+                rateLimit: true,
+                jwksRequestsPerMinute: 5,
+                jwksUri: this.config.jwksUri
+            }),
+            audience: this.config.audience,
+            issuer: this.config.issuer,
+            algorithms: ['RS256'],
+            cookie: cookie
+        });
         return new Router()
             .use(bodyParser() as any)
-            .get('/login',context => {
-                context.response.body = 'login';
+            .get('/login', context => {
+                context.response.body = `
+                <form action="/login" method="post">
+                <input type="text" name="username">
+                <input type="password" name="password">
+                <input type="submit" value="Login">
+                </form>
+                `;
             })
             .post('/login', async context => {
                 const {username, password} = context.request.body;
                 let result = await this.login(username, password);
-                console.log(result);
-                context.response.body = result;
+                context.response.body = 'Logged in';
+                context.cookies.set(cookie, result.id_token);
+                context.redirect('/');
             })
+            .use(async (ctx, next) => {
+                try {
+                    await next();
+                } catch (e) {
+                    console.log(e.message);
+                    throw e;
+                }
+            })
+            .use(checkJwt);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,20 @@ import {AuthenticationClient} from "auth0";
 const {
     AUTH0_CLIENT_ID,
     AUTH0_DOMAIN,
-    AUTH0_CLIENT_SECRET
+    AUTH0_CLIENT_SECRET,
+    AUTH0_JWKS_URI,
+    AUTH0_ISSUER,
+    AUTH0_AUDIENCE
 } = process.env;
 
 let server = new Server(new AuthController(new AuthenticationClient({
-    clientId: AUTH0_CLIENT_ID,
-    clientSecret: AUTH0_CLIENT_SECRET,
-    domain: AUTH0_DOMAIN
-})));
+        clientId: AUTH0_CLIENT_ID,
+        clientSecret: AUTH0_CLIENT_SECRET,
+        domain: AUTH0_DOMAIN
+    }),
+    {
+        audience: AUTH0_AUDIENCE,
+        issuer: AUTH0_ISSUER,
+        jwksUri: AUTH0_JWKS_URI
+    }));
 server.listen();
